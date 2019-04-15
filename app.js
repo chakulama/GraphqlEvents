18880833532b704d2aa32aeb30a0b96d01fdc67e
Binod Lama <binod.lama@es.ey.com>
2019-04-15 10:32:04 +0200

    express server works and basic schema added

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express=  require('express');
 const bodyParser= require ('body-parser');
 
+const graphqlHttp = require ('express-graphql');
+const  { buildSchema } = require ('graphql');
+
 const app= express ();
+
 app.use(bodyParser.json());
-app.get('/',(req,res,next)=>{
-   res.send("Hello World!");
-})
+
+app.use('/graphql',graphqlHttp(
+    {
+    schema:buildSchema(`
+    type RootQuery{
+        events:[String!]
+
+    }
+    type RootMutation{
+        createEvents(name:String):String
+
+    }
+    schema{
+        query:RootQuery
+        mutation:RootMutation
+    }
+    
+    `),
+    rootValue: {
+        events:()=>{
+            
+            return ['Upadate frontend','Make new graphql api'];
+        } ,
+        createEvents:(args)=>{
+            console.log("aaa");
+            const evenName= args.name;
+            
+            return evenName;
+
+        }
+       },
+       graphiql:true
+        }));
 
 app.listen(3000);
